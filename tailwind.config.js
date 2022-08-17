6526a600a208a7849eb119dbe9ff14514ae8b985
SPY_STAR <91047452+SPY-STAR0003@users.noreply.github.com>
2022-08-17 17:01:55 +0430

    add navbar & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        "vazir" : ["Vazirmatn", "serif"]
+      },
+      keyframes : {
+        show : {
+          "0%" : { opacity : "0", transform : "scaleX(0.1)" },
+          "100%" : { opacity : "1", transform : "scaleX(1)" }
+        }
+      },
+      animation : {
+        show : "show .3s ease"
+      }
+    },
   },
   plugins: [],
 }
